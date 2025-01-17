dabb3419f62576625442461d6e934b3b0e1c6b86
Tomas4030 <101940482+Tomas4030@users.noreply.github.com>
2025-01-17 20:22:32 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-function enviarMensagem() {
-    alert("Mensagem enviada com sucesso!");
-        window.location.href = "../Contato.html";
+async function loadNavbar() {
+    const response = await fetch("components/navbar.html");
+    const navbarHtml = await response.text();
+    document.getElementById("navbar").innerHTML = navbarHtml;
 }
+
+async function loadFooter() {
+    const response = await fetch("components/footer.html");
+    const navbarHtml = await response.text();
+    document.getElementById("footer").innerHTML = navbarHtml;
+}
+
+loadFooter();
+loadNavbar();
